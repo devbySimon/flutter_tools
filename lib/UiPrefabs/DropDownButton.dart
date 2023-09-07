2d2d5faeae40331ccc9ee02d788d9fe40ec3c657
Dev_bySimon <devbysimon@gmail.com>
2023-09-07 23:02:47 +0200

    Version 0.0.12
    DropDownButton.dart modified

--- lib/UiPrefabs/DropDownButton.dart
+++ lib/UiPrefabs/DropDownButton.dart
@@ -7,12 +7,22 @@ Widget BySimonDropDownButton(Object? initialValue, List<DropdownMenuItem<Object>
 
   return DecoratedBox(
     decoration: BoxDecoration(
+      gradient: LinearGradient(
+          colors: [
+            Colors.redAccent,
+            Colors.blueAccent,
+            Colors.purpleAccent
+            //add more colors
+          ]),
         borderRadius: BorderRadius.all(Radius.circular(16)),
     ),
-    child: DropdownButton(
-        value: initialValue,
-        items: items,
-        onChanged: onChangedAction
+    child: Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: DropdownButton(
+          value: initialValue,
+          items: items,
+          onChanged: onChangedAction
+      ),
     ),
   );
 }
