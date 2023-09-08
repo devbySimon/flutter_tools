1c7da3eb922829a5beb39743c62017e1627c9302
Dev_bySimon <devbysimon@gmail.com>
2023-09-08 13:43:22 +0200

    Version 0.0.19
    Added BySimonDecoratedBoxFilledNoInnerPadding
    Added BySimonCardColumnWithTextButtonOnSide

--- lib/UiPrefabs/Card.dart
+++ lib/UiPrefabs/Card.dart
@@ -3,6 +3,7 @@ library flutter_tools;
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_tools/UiPrefabs/DecoratedBox.dart';
+import 'package:flutter_tools/flutter_tools.dart';
 
 Widget BySimonCard(BuildContext context, Widget child) {
 
@@ -31,6 +32,33 @@ Widget BySimonCardList(BuildContext context, List<Widget> children) {
       Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: children,
+      )
+  );
+}
+
+Widget BySimonCardColumnWithTextButtonOnSide(BuildContext context, List<Widget> children, String buttonText) {
+
+  return BySimonDecoratedBoxFilledNoInnerPadding(
+      context,
+      Row(
+        children: [
+          Expanded(
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(16)),
+                color: Theme.of(context).colorScheme.surface,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: children,
+                ),
+              ),
+            ),
+          ),
+          SizedBox.fromSize(size: Size.square(4),),
+          BySimonTextTitle(buttonText),
+        ],
       )
   );
 }
--- lib/UiPrefabs/DecoratedBox.dart
+++ lib/UiPrefabs/DecoratedBox.dart
@@ -52,3 +52,22 @@ DecoratedBox BySimonDecoratedBoxFilled(BuildContext context, Widget child) {
       )
   );
 }
+
+DecoratedBox BySimonDecoratedBoxFilledNoInnerPadding(BuildContext context, Widget child) {
+  return DecoratedBox(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+        gradient: LinearGradient(
+            colors: [
+              Theme.of(context).colorScheme.primary,
+              Theme.of(context).colorScheme.secondary,
+              Theme.of(context).colorScheme.tertiary,
+              //add more colors
+            ]),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(4),
+        child: child,
+      )
+  );
+}
