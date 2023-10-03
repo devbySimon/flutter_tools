4df15e8b2ac090f5a057901c7bf14336f05f49c2
Dev_bySimon <devbysimon@gmail.com>
2023-10-03 17:16:01 +0200

    Version 0.0.30
    Added Splitter

--- lib/UiPrefabs/Splitter.dart
+++ lib/UiPrefabs/Splitter.dart
@@ -9,8 +9,9 @@ class BySimonSplitter extends StatefulWidget {
   List<String>? textsToShow;
   List<IconData>? iconsToShow;
   Function(int) onItemSelected;
+  bool withScrolling;
 
-  BySimonSplitter({this.textsToShow, this.iconsToShow, required this.onItemSelected});
+  BySimonSplitter({this.textsToShow, this.iconsToShow, required this.onItemSelected, this.withScrolling = false});
 
   @override
   State<StatefulWidget> createState() => _BySimonSplitterState();
@@ -23,42 +24,51 @@ class _BySimonSplitterState extends State<BySimonSplitter> {
   @override
   Widget build(BuildContext context) {
 
-    return BySimonCard(
-      context,
-      SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
+    Widget child = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
 
-            if (widget.textsToShow != null)
-              for (var text in widget.textsToShow!)
-                TextButton(
-                  child: Text(text, style: TextStyle(color: _currentIndex == widget.textsToShow!.indexOf(text) ?
-                  Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.outline)),
-                  onPressed: () {
-                    setState(() {
-                      _currentIndex = widget.textsToShow!.indexOf(text);
-                    });
-                    widget.onItemSelected(_currentIndex);
-                  },
-                ),
+        if (widget.textsToShow != null)
+          for (var text in widget.textsToShow!)
+            TextButton(
+              child: Text(text, style: TextStyle(color: _currentIndex == widget.textsToShow!.indexOf(text) ?
+              Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.outline)),
+              onPressed: () {
+                setState(() {
+                  _currentIndex = widget.textsToShow!.indexOf(text);
+                });
+                widget.onItemSelected(_currentIndex);
+              },
+            ),
 
-            if (widget.iconsToShow != null)
-              for (var iconData in widget.iconsToShow!)
-                IconButton(onPressed: () {
-                  setState(() {
-                    _currentIndex = widget.iconsToShow!.indexOf(iconData);
-                  });
-                  widget.onItemSelected(_currentIndex);
-                },
-                    icon: Icon(iconData, color: _currentIndex == widget.iconsToShow!.indexOf(iconData) ?
-                    Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.outline)
-                ),
-          ],
-        ),
-      ),
+        if (widget.iconsToShow != null)
+          for (var iconData in widget.iconsToShow!)
+            IconButton(onPressed: () {
+              setState(() {
+                _currentIndex = widget.iconsToShow!.indexOf(iconData);
+              });
+              widget.onItemSelected(_currentIndex);
+            },
+                icon: Icon(iconData, color: _currentIndex == widget.iconsToShow!.indexOf(iconData) ?
+                Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.outline)
+            ),
+      ],
     );
+
+    if (widget.withScrolling) {
+      return BySimonCard(
+        context,
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+            child: child
+        )
+      );
+    } else {
+      return BySimonCard(
+        context,
+        child
+      );
+    }
   }
 
 }
